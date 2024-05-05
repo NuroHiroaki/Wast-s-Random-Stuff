92abfc64d3b597a61c3092f95a200f30643e92a6
NuroHiroaki <168896160+NuroHiroaki@users.noreply.github.com>
2024-05-05 16:19:28 +0800

    Add files via upload

---
+++ Imperishable Day/patch.js
@@ -0,0 +1,7 @@
+{
+    "id": "ImperishableDay(TH11)",
+    "servers": [
+        "https://mirrors.thpatch.net/Wast's Random Stuff/Imperishable Day/"
+    ],
+    "title": "カラスの南の太陽 - Imperishable Day"
+}
