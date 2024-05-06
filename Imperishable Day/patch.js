b5653e7fef018d66a761d4be8bf686aee1503f65
NuroHiroaki <168896160+NuroHiroaki@users.noreply.github.com>
2024-05-06 09:10:31 +0800

    Update patch.js

--- Imperishable Day/patch.js
+++ Imperishable Day/patch.js
@@ -1,7 +1,11 @@
 {
-    "id": "ImperishableDay(TH11)",
-    "servers": [
-        "https://mirrors.thpatch.net/Wast's Random Stuff/Imperishable Day/"
-    ],
-    "title": "カラスの南の太陽 - Imperishable Day"
+"dependencies": [
+"nmlgc/base_tsa",
+"ExpHP/bullet_cap"
+],
+        "id": "ImperishableDay",
+        "servers": [
+        "https://mirrors.thpatch.net/Wast/Imperishable Day/"
+        ],
+        "title": "(TH11) カラスの南の太陽 - Imperishable Day"
 }
